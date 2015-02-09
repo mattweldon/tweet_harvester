76f2ea3d66386585710fd31718c2fe03687a093b
Matt Weldon <matt.weldon@gmail.com>
2015-02-09 07:01:51 +0000

    clean up commented-out code

--- lib/tweet_harvester.ex
+++ lib/tweet_harvester.ex
@@ -7,17 +7,6 @@ defmodule TweetHarvester do
 
   @registry_name TweetHarvesterRegistry
 
-  # def start_workers(sup) do
-  #   # -- Config
-  #   result = {:ok, config_pid} = Supervisor.start_child(sup, worker(TweetHarvesterRegistry, []))
-  #   result
-  # end
-
-  # def init(_) do
-  #   children = [ worker(TweetHarvesterRegistry, []) ]
-  #   supervise([], strategy: :one_for_one)
-  # end  
-
   def init(:ok) do
     children = [
       worker(TweetHarvesterRegistry, [[name: @registry_name]])
